8b307cdb79d681c4705145b1da30e3236b4e9d0b
DanielarenasL <daniel2004al@gmail.com>
2024-06-07 20:49:11 -0500

    Terminado

--- Conectar.py
+++ Conectar.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import pymysql
+import datetime
 
 login_manager = LoginManager()
 app = Flask(__name__)
@@ -9,6 +10,7 @@ login_manager.init_app(app)
 
 class User(UserMixin):
     es_admin = False
+    email = None
 
 @login_manager.user_loader
 def user_loader(email):
@@ -20,6 +22,7 @@ def user_loader(email):
         return
     user = User()
     user.id = email
+    user.email = email
     user.es_admin = user_db[1]
     return user
 
@@ -88,11 +91,28 @@ def protected():
 def inicio():
     miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
     cur = miConexion.cursor()
-    cur.execute('SELECT Nombre_Producto FROM Producto')
+    cur.execute('SELECT Id_Producto, Nombre_Producto, Precio_Producto, url_producto FROM Producto')  
     productos = cur.fetchall()
     miConexion.close()
 
     return render_template('inicio.html', productos=productos)
+
+@app.route('/descripcion/<int:producto_id>')
+def descripcion(producto_id):
+    miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
+    cur = miConexion.cursor()
+    cur.execute('SELECT * FROM Producto WHERE Id_Producto = %s', (producto_id,))
+    producto = cur.fetchone()
+
+    cur.execute('SELECT * FROM Comentarios WHERE Id_Producto = %s', (producto_id,))
+    comentarios = cur.fetchall()
+
+    miConexion.close()
+
+    if producto is None:
+        return 'Producto no encontrado', 404
+
+    return render_template('descripcion.html', producto=producto, comentarios=comentarios)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -147,12 +167,32 @@ def admin():
 @app.route('/carrito')
 @login_required
 def carrito():
-    return render_template('carrito.html')
+    miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
+    cur = miConexion.cursor()
+    cur.execute('SELECT Id_Carrito FROM Carrito WHERE Correo_Usuario = %s', (current_user.email,))
+    carrito_id = cur.fetchone()[0]
+    cur.execute('SELECT Producto.Id_Producto, Nombre_Producto, Precio_Producto, url_producto, Cantidad_Producto FROM Producto JOIN Items_Carrito ON Producto.Id_Producto = Items_Carrito.Id_Producto WHERE Id_Carrito = %s', (carrito_id,))
+    productos = cur.fetchall()
+    miConexion.close()
 
-@app.route('/comprar', methods=['POST'])
+    return render_template('carrito.html', productos=productos)
+
+@app.route('/pagar', methods=['POST'])
 @login_required
-def comprar():
-    return 'La compra fue realizada'
+def pagar():
+    miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
+    cur = miConexion.cursor()
+
+    cur.execute('SELECT Id_Carrito FROM Carrito WHERE Correo_Usuario = %s', (current_user.email,))
+    carrito_id = cur.fetchone()[0]
+
+    cur.execute('SELECT SUM(Producto.Precio_Producto * Items_Carrito.Cantidad_Producto) FROM Producto JOIN Items_Carrito ON Producto.Id_Producto = Items_Carrito.Id_Producto WHERE Id_Carrito = %s', (carrito_id,))
+    total = cur.fetchone()[0]
+
+    miConexion.close()
+
+    return 'Compra exitosa! Tu total fue: $' + str(total)
+
 
 @app.route('/agregar_mercancia', methods=['GET'])
 @login_required
@@ -173,10 +213,11 @@ def agregar_mercancia_post():
         talla_producto = request.form['talla_producto']
         color_producto = request.form['color_producto']
         cantidad_producto = request.form['cantidad_producto']
+        url_producto = request.form['url_producto']
 
         miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
         cur = miConexion.cursor()
-        cur.execute('INSERT INTO Producto (Nombre_Producto, Descripcion_Producto, Precio_Producto, Tipo_Producto, Talla_Producto, Color_Producto, Cantidad) VALUES (%s, %s, %s, %s, %s, %s, %s)', (nombre_producto, descripcion_producto, precio_producto, tipo_producto, talla_producto, color_producto, cantidad_producto))
+        cur.execute('INSERT INTO Producto (Nombre_Producto, Descripcion_Producto, Precio_Producto, Tipo_Producto, Talla_Producto, Color_Producto, Cantidad, url_producto) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (nombre_producto, descripcion_producto, precio_producto, tipo_producto, talla_producto, color_producto, cantidad_producto, url_producto))
         miConexion.commit()
         miConexion.close()
 
@@ -291,6 +332,56 @@ def actualizar_mercancia_post():
         return 'La mercancía ha sido actualizada exitosamente'
     else:
         return 'No tienes permiso para acceder a esta página'
+
+@app.route('/agregar_al_carrito/<int:producto_id>', methods=['POST'])
+@login_required
+def agregar_al_carrito(producto_id):
+    miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
+    cur = miConexion.cursor()
+
+    cur.execute('SELECT Id_Carrito FROM Carrito WHERE Correo_Usuario = %s', (current_user.email,))
+    carrito = cur.fetchone()
+
+    if carrito is None:
+        cur.execute('INSERT INTO Carrito (Correo_Usuario) VALUES (%s)', (current_user.email,))
+        carrito_id = cur.lastrowid
+    else:
+        carrito_id = carrito[0]
+    cur.execute('INSERT INTO Items_Carrito (Id_Carrito, Id_Producto, Cantidad_Producto) VALUES (%s, %s, 1)', (carrito_id, producto_id))
+
+    miConexion.commit()
+    miConexion.close()
+
+    return 'Producto añadido al carrito', 200
+
+@app.route('/eliminar_del_carrito/<int:producto_id>', methods=['POST'])
+@login_required
+def eliminar_del_carrito(producto_id):
+    miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
+    cur = miConexion.cursor()
+    cur.execute('SELECT Id_Carrito FROM Carrito WHERE Correo_Usuario = %s', (current_user.email,))
+    carrito_id = cur.fetchone()[0]
+    cur.execute('DELETE FROM Items_Carrito WHERE Id_Carrito = %s AND Id_Producto = %s', (carrito_id, producto_id))
+
+    miConexion.commit()
+    miConexion.close()
+
+    return redirect(url_for('carrito'))
+
+@app.route('/comentar/<int:producto_id>', methods=['POST'])
+@login_required
+def comentar(producto_id):
+    comentario = request.form['comment']
+    correo_usuario = current_user.email
+    fecha = datetime.datetime.now()
+
+    miConexion = pymysql.connect(host ='localhost',user ='root',passwd ='123daniel...',db = 'proyecto')
+    cur = miConexion.cursor()
+    cur.execute('INSERT INTO Comentarios (Id_Producto, Correo_Usuario, Comentario, Fecha) VALUES (%s, %s, %s, %s)', (producto_id, correo_usuario, comentario, fecha))
+    miConexion.commit()
+    miConexion.close()
+
+    return redirect(url_for('descripcion', producto_id=producto_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
